d4b26155450a09027ce424bbc10214940c496a3b
Carter Prince <carteraprince@gmail.com>
2024-04-03 12:40:24 -0400

    Update blogspot_fetch.py

--- blogspot_fetch.py
+++ blogspot_fetch.py
@@ -3,67 +3,84 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
+import pdfkit
 
 if len(sys.argv) != 2:
-    print("Usage: blogspot_fetch.py <url>")
+    print("Usage: blogspot_fetch.py <blog_index_url>")
     exit(1)
 
 url = sys.argv[1]
+blog_name = url.split(".")[0].split("/")[-1]
 
-# Send a GET request to the URL
 response = requests.get(url)
 
-# Create a BeautifulSoup object to parse the HTML
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Find all links matching the specified CSS selector
-links = [link["href"] for link in soup.select("#post-body-4425880098561414349 > p > a")]
+links = [link["href"] for link in soup.select(".post-body > p > a")]
 
-# Create an empty list to store the article titles
 titles = []
 
 output_html = ""
 separator_html = "<div class=\"page-separator\"></div>\n"
 
-# Iterate over each link
-for i, link in enumerate(links, start=1):
-    print(f"Fetching {link}")
-    # Send a GET request to each link
+article = 1
+for link in links:
     link_response = requests.get(link)
-    # Create a BeautifulSoup object for the link's content
+
+    if "Sensitive Content Warning" in link_response.text:
+        print(f"Unable to fetch {link} (sensitive content)")
+        continue
+    else:
+        print(f"Fetched {link}")
+
     link_soup = BeautifulSoup(link_response.text, "html.parser")
-    # Extract the article title
+
     title_element = link_soup.select_one("#Blog1 > div > article > div > div > h3")
     if title_element:
         titles.append(title_element.text.strip())
-        title_element['id'] = f"article-{i}"
-    # Delete the specified elements
+        title_element['id'] = f"article-{article}"
+
     share_button = link_soup.select_one("#Blog1 > div > article > div > div > div.post-share-buttons.post-share-buttons-top")
     if share_button:
         share_button.decompose()
+
     post_bottom = link_soup.select_one("#Blog1 > div > article > div > div > div.post-bottom")
     if post_bottom:
         post_bottom.decompose()
-    # Get the desired element from the link's content
+
     desired_element = link_soup.select_one("#Blog1 > div > article > div > div")
-    # Append the element's HTML text to the output file
     if desired_element:
         output_html += desired_element.prettify() + "\n"
         output_html += separator_html
 
+    article += 1
+
 with open("style.html", "r") as file:
     style_html = file.read()
 
-# Create the table of contents HTML
 toc_html = "<h3 class=\"post-title entry-title\"> Table of Contents</h3>\n<ul>\n"
 for i, title in enumerate(titles, start=1):
     toc_html += f'<li><a href="#article-{i}">{title}</a></li>\n'
-toc_html += "</ul>\n"
+toc_html += "</ul>" + separator_html
 
-# Write the output file with the table of contents and output HTML
-with open("output.html", "w", encoding="utf-8") as file:
+with open(f"{blog_name}.html", "w", encoding="utf-8") as file:
     file.write(style_html)
     file.write(toc_html)
     file.write(output_html)
 
-print("Wrote to output.html")
+print(f"Wrote to {blog_name}.html")
+
+options = {
+    'page-size': 'Letter',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+    'encoding': 'UTF-8'
+}
+
+print(f"Generating {blog_name}.pdf...")
+
+pdfkit.from_file(f"{blog_name}.html", f"{blog_name}.pdf", options=options)
+
+print(f"Generated {blog_name}.pdf")
